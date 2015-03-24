f7216ea8de58d7ca9c3dc2d4740488993a6c696c
叶柳 <yeliu@meizu.com>
2015-03-24 16:33:59 +0800

    [Hook]提交CPP注入代码

---
+++ sample/hook_hello_cpp/Hook.cpp
@@ -0,0 +1,157 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <android/log.h>
+#include <utils/Log.h>
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <hardware_legacy/AudioPolicyManagerBase.h>
+
+#include "Hook.h"
+
+namespace android_audio_legacy {
+
+// ----------------------------------------------------------------------------
+// Hook implementation
+// ----------------------------------------------------------------------------
+
+void* get_module_base(pid_t pid, const char* module_name)
+{
+    FILE *fp;
+    long addr = 0;
+    char *pch;
+    char filename[32];
+    char line[1024];
+
+    if (pid < 0) {
+        /* self process */
+        snprintf(filename, sizeof(filename), "/proc/self/maps");
+    } else {
+        snprintf(filename, sizeof(filename), "/proc/%d/maps", pid);
+    }
+
+    fp = fopen(filename, "r");
+
+    if (fp != NULL) {
+        while (fgets(line, sizeof(line), fp)) {
+            if (strstr(line, module_name)) {
+                pch = strtok( line, "-" );
+                addr = strtoul( pch, NULL, 16 );
+
+                if (addr == 0x8000)
+                    addr = 0;
+
+                break;
+            }
+        }
+
+        fclose(fp);
+    }
+
+    return (void *)addr;
+}
+
+int hook_eglSwapBuffers(char * pcTargetLib)
+{
+    int nRet = -1;
+    int nFd = -1;
+
+    nFd = open(pcTargetLib, O_RDONLY);
+    if (-1 == nFd) {
+        ALOGD("[+] Open taget library error\n");
+        nRet = -1;
+    }
+
+    void * base_addr = get_module_base(getpid(), pcTargetLib);
+    ALOGD("[+] Target Library address = %p\n", base_addr);
+
+    Elf32_Ehdr ehdr;
+    read(nFd, &ehdr, sizeof(Elf32_Ehdr));
+
+    unsigned long shdr_addr = ehdr.e_shoff; // 节区头部表格相对文件开头的偏移量（按字节计算）
+    int shnum = ehdr.e_shnum;               // 节区头部表格的表项数目
+    int shent_size = ehdr.e_shentsize;      // 节区头部表格的表项大小
+    unsigned long stridx = ehdr.e_shstrndx; // 节区头部表格与节区名称字符串表相关的表项的索引
+
+    /*
+     *节区头部数据结构描述
+     *
+     *typedef struct {
+     *   Elf32_Word sh_name;                // 节区名称
+     *   Elf32_Word sh_type;                // 节区类型
+     *   Elf32_Word sh_flags;               // 标志描述属性
+     *   Elf32_Word sh_addr;                // 节区出现在内存映像中时给出节区第一个字节应处的位置
+     *   Elf32_Word sh_offset;              // 节区第一字节与文件头之间的偏移
+     *   Elf32_Word sh_size;                // 节区的长度
+     *   Elf32_Word sh_link;                // 节区头部索引链接
+     *   Elf32_Word sh_info;                // 附加信息
+     *   Elf32_Word sh_addralign;           // 某些节区带有地址对齐约束
+     *   Elf32_Word sh_entsize;             // 某些节区中包含固定大小的项目，如符号表
+     *}Elf32_Shdr
+    */
+
+    Elf32_Shdr shdr;                        // 节区头部
+    lseek(nFd, shdr_addr + stridx * shent_size, SEEK_SET);   // 节区头部表格起始地址 + stridx * shent_size = .shstrtab（包含各节区名称）
+    read(nFd, &shdr, shent_size);
+
+    char * string_table = (char *)malloc(shdr.sh_size);
+    lseek(nFd, shdr.sh_offset, SEEK_SET);
+    read(nFd, string_table, shdr.sh_size);
+    lseek(nFd, shdr_addr, SEEK_SET);
+
+    int i;
+    uint32_t out_addr = 0;
+    uint32_t out_size = 0;
+    uint32_t got_item = 0;
+    int32_t got_found = 0;
+
+    for (i = 0; i < shnum; i++) {
+        read(nFd, &shdr, shent_size);
+        if (shdr.sh_type == SHT_PROGBITS) { // SHT_PROGBITS：表明此节区包含程序定义的信息，其格式和含义都由程序解析
+            int name_idx = shdr.sh_name;
+            if (strcmp(&(string_table[name_idx]), ".got.plt") == 0 || strcmp(&(string_table[name_idx]), ".got") == 0) {
+                out_addr = (uint32_t)(base_addr + shdr.sh_addr);
+                out_size = shdr.sh_size;
+                ALOGD("[+] Got section start_addr = %lx, section_size = %lx\n", out_addr, out_size);
+
+                for (i = 0; i < out_size; i += 4) {
+                    got_item = *(uint32_t *)(out_addr + i);
+                    if (got_item  == got_item) {
+                        ALOGD("[+] Found eglSwapBuffers in got section\n");
+                        got_found = 1;
+
+                        uint32_t page_size = getpagesize();                                         // 获取分页大小
+                        uint32_t entry_page_start = (out_addr + i) & (~(page_size - 1));
+                        mprotect((uint32_t *)entry_page_start, page_size, PROT_READ | PROT_WRITE);  // 设置内存访问权限
+                        //*(uint32_t *)(out_addr + i) = new_eglSwapBuffers;
+
+                        break;
+                    } else if (got_item == got_item) {
+                        ALOGD("Already hooked\n");
+                        break;
+                    }
+                }
+                if (got_found)
+                    break;
+            }
+        }
+    }
+
+    free(string_table);
+    close(nFd);
+
+    return nRet;
+}
+
+int hook_entry(char *pcTargetLib)
+{
+    ALOGD("[+] Start hooking %s\n", pcTargetLib);
+    // old_eglSwapBuffers = eglSwapBuffers;
+    // hook_eglSwapBuffers(pcTargetLib);
+    return 0;
+}
+
+};
---
+++ sample/hook_hello_cpp/Hook.h
@@ -0,0 +1,1 @@
+extern "C" int hook_entry(char *pcTargetLib);
