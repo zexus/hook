3a8fd749dec3c4d6f282a505629d14a0223f77d7
叶柳 <yeliu@meizu.com>
2015-03-25 12:14:43 +0800

    [Hook]修改测试用例

--- sample/hook_library/library/demo_hook.c
+++ sample/hook_library/library/demo_hook.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int Hook_Entry()
 {
-    printf("Hook_Entry++++++++++++++++++++++++++++++++\n");
+    printf("Hook_Entry++++++++++++++++++++++++++++++++%d\n", getpid()); // _getpid()
     return 0;
 }
 
 int foo() {
-    printf("foo called++++++++++++++++++++++++++++++++++\n");
+    printf("foo called++++++++++++++++++++++++++++++++++%d\n", getpid());   // _getpid()
     return 0;
 }
