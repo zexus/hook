cccb801230ab3dec9183272cf2d50c959eae0143
叶柳 <yeliu@meizu.com>
2015-03-31 15:49:11 +0800

    [Hook]修改测试用例

--- sample/hook/test/hook.c
+++ sample/hook/test/hook.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int foo(int nValue1, int nValue2, int nValue3, int nValue4, int nValue5, int nValue6) {
+int foo(char * pcString, int nValue1, int nValue2, int nValue3, int nValue4, int nValue5, int nValue6) {
     printf("foo called++++++++++++++++++++++++++++++++++%d\n", getpid());   // _getpid()
+    printf("pcString+++++++++++++++++++++++++++++++++%s\n", pcString);
     printf("nValue1++++++++++++++++++++++++++++++++++%d\n", nValue1);
     printf("nValue2++++++++++++++++++++++++++++++++++%d\n", nValue2);
     printf("nValue3++++++++++++++++++++++++++++++++++%d\n", nValue3);
