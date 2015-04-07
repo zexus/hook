0031563c5c517d6e432a965fb365d355c30bf8f4
叶柳 <yeliu@meizu.com>
2015-04-07 16:37:09 +0800

    [Hook]添加数值和地址打印

--- sample/hook/execute/demo_main.c
+++ sample/hook/execute/demo_main.c
@@ -3,8 +3,15 @@
 
 int main()
 {
+    int nTarget1 = 1;
+    int nTarget2 = 2;
+
     while (1) {
         Hook_Entry_Test("Welcome", 9, 9, 9, 9, 9, 9);
+        printf("nTarget1+++++++++++++++++++++++++++++++++%d\n", nTarget1);
+        printf("nTarget1+++++++++++++++++++++++++++++++++%p\n", &nTarget1);
+        printf("nTarget2+++++++++++++++++++++++++++++++++%d\n", nTarget2);
+        printf("nTarget2+++++++++++++++++++++++++++++++++%p\n", &nTarget2);
         sleep(1);
 
         // void * pcMapAddr = mmap(0, 0x4000, PROT_READ | PROT_WRITE | PROT_EXEC, 0x20 | MAP_PRIVATE, 0, 0);
