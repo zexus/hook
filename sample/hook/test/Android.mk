c76606a9c3cb389aae6f8fa3ed51e0d97f47061e
叶柳 <yeliu@meizu.com>
2015-04-13 17:51:45 +0800

    [Hook]添加编译标志，解决编译报错的问题

--- sample/hook/test/Android.mk
+++ sample/hook/test/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libhook
 
-LOCAL_LDFLAGS := -ldemo_hook
+LOCAL_LDFLAGS := -ldemo_hook -llog
 
-LOCAL_LDLIBS := -ldemo_hook
+LOCAL_LDLIBS := -ldemo_hook -llog
 
 LOCAL_SRC_FILES := hook.c
 
