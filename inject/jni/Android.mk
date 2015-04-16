2f89fabcd153a50a24c31f9d679193d1174f5da8
叶柳 <yeliu@meizu.com>
2015-04-16 17:23:39 +0800

    [Hook]整理代码

--- inject/jni/Android.mk
+++ inject/jni/Android.mk
@@ -2,11 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := inject
-
-LOCAL_SRC_FILES :=  inject.c \
-                    elf_utils.c \
-                    ptrace_utils.c
+LOCAL_SRC_FILES := \
+	inject.c \
+	elf_utils.c \
+	ptrace_utils.c
 
 LOCAL_CFLAGS += -pie -fPIE
 
@@ -14,6 +13,6 @@ LOCAL_LDFLAGS += -pie -fPIE
 
 LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog -ldl
 
-#LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE := inject
 
 include $(BUILD_EXECUTABLE)
