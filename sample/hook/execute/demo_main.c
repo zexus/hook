d7ad4d695b4da1c48fd33d06fa64d22207fb91da
叶柳 <yeliu@meizu.com>
2015-04-16 16:48:37 +0800

    [Hook]修改测试用例

--- sample/hook/execute/demo_main.c
+++ sample/hook/execute/demo_main.c
@@ -3,21 +3,9 @@
 
 int main()
 {
-    int nTarget1 = 1;
-    int nTarget2 = 2;
-
     while (1) {
         Hook_Entry_Test("Welcome", 9, 9, 9, 9, 9, 9);
         sleep(1);
-
-        // void * pcMapAddr = mmap(0, 0x4000, PROT_READ | PROT_WRITE | PROT_EXEC, 0x20 | MAP_PRIVATE, 0, 0);
-        // if ((void *)-1) == pcMapAddr) {
-        //      printf("mmap memory error.\n");
-        //  } else {
-        //      printf("pcMapAddr:+++++++++++++++%p\n", pcMapAddr);
-        //      munmap(pcMapAddr, 0x4000);
-        //  }
-        // foo();
     }
 
     return 0;
