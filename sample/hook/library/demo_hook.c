9f6df06728c8acf585e7cb370ff2d6149308316a
叶柳 <yeliu@meizu.com>
2015-04-16 16:52:15 +0800

    [Hook]修改测试用例

--- sample/hook/library/demo_hook.c
+++ sample/hook/library/demo_hook.c
@@ -4,7 +4,6 @@
 volatile int Hook_Entry_Test(char * pcString, int nValue1, int nValue2, int nValue3, int nValue4, int nValue5, int nValue6)
 {
     printf("Hook_Entry_Test++++++++++++++++++++++++++%d\n", getpid()); // _getpid()
-    printf("pcString+++++++++++++++++++++++++++++++++%s\n", pcString);
     return 0;
 }
 
