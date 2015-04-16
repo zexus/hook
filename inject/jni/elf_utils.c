a57463ebaf75ec2d1613bd2a23252d3feabc9f39
叶柳 <yeliu@meizu.com>
2015-04-16 14:51:03 +0800

    [Hook]添加WRITE BYTES函数

--- inject/jni/elf_utils.c
+++ inject/jni/elf_utils.c
@@ -71,6 +71,58 @@ int ptrace_read_bytes(pid_t pid, const unsigned long *src, void *buf, size_t siz
 	return 0;
 }
 
+int ptrace_write_bytes(pid_t pid, unsigned long *dst, const void *buf, size_t size)
+{
+	int wordCount, byteRemain;
+	unsigned long *dstAddr;
+	const long *dataAddr;
+	WORDUN un;
+	int i;
+
+	if (dst == NULL || buf == NULL || size < 0)
+	{
+		DEBUG_PRINT("[+] ptrace_write_bytes invalid params\n");
+		return EINVAL;
+	}
+
+	wordCount = size / sizeof(long);
+	byteRemain = size % sizeof(long);
+	dstAddr = dst;
+	dataAddr = buf;
+
+	for (i = 0; i < wordCount; i++, dstAddr++, dataAddr++)
+	{
+		if (ptrace(PTRACE_POKEDATA, pid, dstAddr, (void*) (*dataAddr)) != 0)
+		{
+			DEBUG_PRINT("POKEDATA to 0x%lx failed %d\n", dstAddr, errno);
+			return errno;
+		}
+	}
+
+	if (byteRemain > 0)
+	{
+		un.value = ptrace(PTRACE_PEEKDATA, pid, dstAddr, NULL);
+		if (errno != 0)
+		{
+			DEBUG_PRINT("PEEKDATA in write bytes failed %d\n", errno);
+			return errno;
+		}
+
+		for (i = 0; i < byteRemain; i++)
+		{
+			un.chars[i] = ((char*) dataAddr)[i];
+		}
+
+		if (ptrace(PTRACE_POKEDATA, pid, dstAddr, (void*) (un.value)) != 0)
+		{
+			DEBUG_PRINT("POKEDATA 0x%lx failed %d\n", dstAddr, errno);
+			return errno;
+		}
+	}
+
+	return 0;
+}
+
 int ptrace_strlen(pid_t pid, const unsigned long *addr)
 {
 	int len = 0;
