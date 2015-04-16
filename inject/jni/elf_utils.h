428f189120a22a3db7977d24e8413d00027b198e
叶柳 <yeliu@meizu.com>
2015-04-16 17:32:20 +0800

    [Hook]整理代码

--- inject/jni/elf_utils.h
+++ inject/jni/elf_utils.h
@@ -1,1 +1,6 @@
+#ifndef ELF_UTIL_H_
+#define ELF_UTIL_H_
+
 int find_func_by_got(pid_t pid, const char* name, unsigned long* entry_addr, unsigned long* entry_value);
+
+#endif /* ELF_UTIL_H_ */
--- inject/jni/ptrace_utils.h
+++ inject/jni/ptrace_utils.h
@@ -1,3 +1,6 @@
+#ifndef PTRACE_H_
+#define PTRACE_H_
+
 int ptrace_getregs(pid_t pid, struct pt_regs * regs);
 int ptrace_setregs(pid_t pid, struct pt_regs * regs);
 int ptrace_attach(pid_t pid);
@@ -8,3 +11,5 @@ long ptrace_ip(struct pt_regs * regs);
 int ptrace_readdata(pid_t pid,  uint8_t *src, uint8_t *buf, size_t size);
 int ptrace_writedata(pid_t pid, uint8_t *dest, uint8_t *data, size_t size);
 int ptrace_call(pid_t pid, uint32_t addr, long *params, uint32_t num_params, struct pt_regs* regs);
+
+#endif /* PTRACE_H_ */
