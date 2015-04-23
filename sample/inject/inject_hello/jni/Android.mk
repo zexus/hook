f3169f1ebc2c84015185df6677065b38493f21ea
叶柳 <yeliu@meizu.com>
2015-04-23 15:15:23 +0800

    [HOOK]目录整理

--- sample/inject/inject_hello/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
-#LOCAL_ARM_MODE := arm
-LOCAL_MODULE    := hook
-LOCAL_SRC_FILES := hook.c
-include $(BUILD_SHARED_LIBRARY)
